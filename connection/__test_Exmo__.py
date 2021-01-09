43b95a8c9c62a216f3b241e432ff6e6b71337488
Alexey Perestoronin <Alexey.Perestoronin@acronis.com>
2021-01-09 18:04:12 +0300

    modifying connection module

--- connection/__test_Exmo__.py
+++ connection/__test_Exmo__.py
@@ -1,4 +1,5 @@
 import os
+import re
 import sys
 import json
 import unittest
@@ -11,29 +12,89 @@ from connection import Exmo
 
 class Test_Exmo(unittest.TestCase):
     def setUp(self):
-        pass
+        publick_key = faf.GetFileContent(faf.SearchAllFilesFromRoot2(os.getcwd(), re.compile(r"^.+?\.publick\.key$"))[0])
+        secret_key = bytes(faf.GetFileContent(faf.SearchAllFilesFromRoot2(os.getcwd(), re.compile(r"^.+?\.secret\.key$"))[0]), encoding="utf-8")
+        self.__exmo = Exmo()
+        self.__exmo.SetPublickKey(publick_key)
+        self.__exmo.SetSecretKey(secret_key)
 
     @staticmethod
-    def test_GetTrades():
+    def GetTrades():
         faf.SaveContentToFile2(faf.SplitPath1(sys.argv[0]), "GetTrades-BTC_USD.log", json.dumps(Exmo.GetTrades(pair="BTC_USD"), indent=4))
         faf.SaveContentToFile2(faf.SplitPath1(sys.argv[0]), "GetTrades-USDT_RUB.log", json.dumps(Exmo.GetTrades(pair="USDT_RUB"), indent=4))
 
     @staticmethod
-    def test_GetOrderBook():
+    def GetOrderBook():
         faf.SaveContentToFile2(faf.SplitPath1(sys.argv[0]), "GetOrderBook-BTC_USD.log", json.dumps(Exmo.GetOrderBook(pair="BTC_USD"), indent=4))
         faf.SaveContentToFile2(faf.SplitPath1(sys.argv[0]), "GetOrderBook-USDT_RUB.log", json.dumps(Exmo.GetOrderBook(pair="USDT_RUB"), indent=4))
 
     @staticmethod
-    def test_GetTicker():
+    def GetTicker():
         faf.SaveContentToFile2(faf.SplitPath1(sys.argv[0]), "GetTicker.log", json.dumps(Exmo.GetTicker(), indent=4))
 
     @staticmethod
-    def test_GetPairSettings():
+    def GetPairSettings():
         faf.SaveContentToFile2(faf.SplitPath1(sys.argv[0]), "GetPairSettings.log", json.dumps(Exmo.GetPairSettings(), indent=4))
 
     @staticmethod
-    def test_GetCurrencyList():
+    def GetCommissionForPair():
+        faf.SaveContentToFile2(faf.SplitPath1(sys.argv[0]), "GetCommissionForPair-BTC_USD.log", json.dumps(Exmo.GetCommissionForPair("BTC_USD"), indent=4))
+        faf.SaveContentToFile2(faf.SplitPath1(sys.argv[0]), "GetCommissionForPair-BTC_USDT.log", json.dumps(Exmo.GetCommissionForPair("BTC_USDT"), indent=4))
+
+    @staticmethod
+    def GetCurrencyList():
         faf.SaveContentToFile2(faf.SplitPath1(sys.argv[0]), "GetCurrencyList.log", json.dumps(Exmo.GetCurrencyList(), indent=4))
+
+    def GetUserInfo(self):
+        faf.SaveContentToFile2(faf.SplitPath1(sys.argv[0]), "GetUserInfo.log", json.dumps(self.__exmo.GetUserInfo(), indent=4))
+
+    def GetUserBalance(self):
+        faf.SaveContentToFile2(faf.SplitPath1(sys.argv[0]), "GetUserBalance-RUB.log", json.dumps(self.__exmo.GetUserBalance("RUB"), indent=4))
+        faf.SaveContentToFile2(faf.SplitPath1(sys.argv[0]), "GetUserBalance-USDT.log", json.dumps(self.__exmo.GetUserBalance("USDT"), indent=4))
+
+    def GetUserOpenOrders(self):
+        faf.SaveContentToFile2(faf.SplitPath1(sys.argv[0]), "GetUserOpenOrders.log", json.dumps(self.__exmo.GetUserOpenOrders(), indent=4))
+
+    def GetUserCancelledOrders(self):
+        faf.SaveContentToFile2(faf.SplitPath1(sys.argv[0]), "GetUserCancelledOrders.log", json.dumps(self.__exmo.GetUserCancelledOrders(), indent=4))
+
+    def GetUserDeals(self):
+        faf.SaveContentToFile2(faf.SplitPath1(sys.argv[0]), "GetUserDeals-USDT_RUB.log", json.dumps(self.__exmo.GetUserDeals("USDT_RUB"), indent=4))
+        faf.SaveContentToFile2(faf.SplitPath1(sys.argv[0]), "GetUserDeals-BTC_USDT.log", json.dumps(self.__exmo.GetUserDeals("BTC_USDT"), indent=4))
+
+    def GetOrderDeals(self):
+        faf.SaveContentToFile2(faf.SplitPath1(sys.argv[0]), "GetOrderDeals-sell-order.log", json.dumps(self.__exmo.GetOrderDeals("11589112646"), indent=4))
+        faf.SaveContentToFile2(faf.SplitPath1(sys.argv[0]), "GetOrderDeals-buy-order.log", json.dumps(self.__exmo.GetOrderDeals("11606216514"), indent=4))
+        faf.SaveContentToFile2(faf.SplitPath1(sys.argv[0]), "GetOrderDeals-buy-match.log", json.dumps(self.__exmo.GetOrderDeals("11654835769"), indent=4))
+
+    def CreateOrder_Buy_Check_Cancel(self):
+        order_id = self.__exmo.CreateOrder_Buy("USDT_RUB", 1, 50)
+        self.assertTrue(self.__exmo.IsOrderOpen(order_id))
+        self.__exmo.CancelOrder(order_id)
+        self.assertFalse(self.__exmo.IsOrderOpen(order_id))
+
+    def CreateOrder_BuyTotal_Check_Cancel(self):
+        order_id = self.__exmo.CreateOrder_BuyTotal("USDT_RUB", 100, 50)
+        self.assertTrue(self.__exmo.IsOrderOpen(order_id))
+        self.__exmo.CancelOrder(order_id)
+        self.assertFalse(self.__exmo.IsOrderOpen(order_id))
+
+    def test_PerformTest(self):
+        # self.GetTrades()
+        # self.GetOrderBook()
+        # self.GetTicker()
+        # self.GetPairSettings()
+        # self.GetCommissionForPair()
+        # self.GetCurrencyList()
+        # self.GetUserInfo()
+        # self.GetUserBalance()
+        # self.GetUserOpenOrders()
+        # self.GetUserCancelledOrders()
+        self.GetUserDeals()
+        # self.GetOrderDeals()
+        # self.CreateOrder_Buy_Check_Cancel()
+        # self.CreateOrder_BuyTotal_Check_Cancel()
+        pass
 
 if __name__ == "__main__":
     unittest.main()
--- connection/_exmo.py
+++ connection/_exmo.py
@@ -11,11 +11,192 @@ class Exmo:
     url = "api.exmo.com"
     api_version = "v1.1"
 
-    def __init__(self, publick_key, secret_key):
-        self.__publick_key = publick_key
-        self.__secret_key = secret_key
+    def __init__(self):
+        self._publick_key = None
+        self._secret_key = None
+
+    # brief: performs https-get/post requests to private-API of Exmo-exchange
+    # param: api_method - the requested method
+    # param: params - params for the specifing of the method
+    # return: json-string with response on the requested method
+    def _QueryPrivate(self, api_method, **params):
+        return Exmo.Query(api_method, self._publick_key, self._secret_key, **params)
+
+    # brief: sets the publick-key for current connection
+    # param: publick_key - target publick-key
+    def SetPublickKey(self, publick_key):
+        self._publick_key = publick_key
+
+    # brief: sets the secret-key for current connection
+    # param: publick_key - target secret-key
+    def SetSecretKey(self, secret_key):
+        self._secret_key = secret_key
 
     # NOTE: for detail information about all below class-methods see https://documenter.getpostman.com/view/10287440/SzYXWKPi#public_api?utm_source=site&utm_medium=articles%22
+
+    # brief: gets information about user's account
+    def GetUserInfo(self):
+        return self._QueryPrivate("user_info")
+
+    def GetUserBalance(self, currency):
+        return float(self.GetUserInfo()["balances"][currency])
+
+    # brief: gets the list of user's open (active) orders
+    def GetUserOpenOrders(self):
+        return self._QueryPrivate("user_open_orders")
+
+    # brief: gets the list of user's candelled orders
+    def GetUserCancelledOrders(self, limit=100, offset=0):
+        return self._QueryPrivate("user_cancelled_orders", limit=limit, offset=offset)
+
+    # brief: gets the list of user's deals
+    def GetUserDeals(self, pair, limit=100, offset=0):
+        return self._QueryPrivate("user_trades", pair=pair, limit=limit, offset=offset)
+
+    # brief: gets the list of trade's deals
+    def GetOrderDeals(self, order_id):
+        return self._QueryPrivate("order_trades", order_id=order_id)
+
+    # brief: creates an buy-order
+    def CreateOrder_Buy(self, pair, buy_quantity, buy_rate):
+        return self._QueryPrivate("order_create", type="buy", pair=pair, quantity=buy_quantity, price=buy_rate)["order_id"]
+
+    # brief: creates an buy-order at predefined total-cost
+    def CreateOrder_BuyTotal(self, pair, total_cost, buy_rate):
+        buy_quantity = total_cost / buy_rate
+        return self._QueryPrivate("order_create", type="buy", pair=pair, quantity=buy_quantity, price=buy_rate)["order_id"]
+
+    # brief: creates an buy-order at current market buy-rate
+    def CreateOrder_BuyMarket(self, pair, buy_quantity):
+        return self._QueryPrivate("order_create", type="market_buy", pair=pair, quantity=buy_quantity, price=0)["order_id"]
+
+    # brief: creates an buy-order at current market buy-rate and predefined total-cost
+    def CreateOrder_BuyMarketTotal(self, pair, total_cost):
+        return self._QueryPrivate("order_create", type="market_buy_total", pair=pair, quantity=total_cost, price=0)["order_id"]
+
+    # brief: creates an sell-order
+    def CreateOrder_Sell(self, pair, sell_quantity, sell_rate):
+        return self._QueryPrivate("order_create", type="sell", pair=pair, quantity=sell_quantity, price=sell_rate)["order_id"]
+
+    # brief: creates an sell-order for selling all target currency
+    # note1: if pair is "BTC_USD" the target currency is "BTC"
+    def CreateOrder_SellAll(self, pair, sell_rate):
+        sell_quantity = self.GetUserBalance(Exmo._1FromPair(pair))
+        return self._QueryPrivate("order_create", type="sell", pair=pair, quantity=sell_quantity, price=sell_rate)["order_id"]
+
+    # brief: creates an sell-order for selling all target currency that is more saved quantity
+    # note1: if pair is "BTC_USD" the target currency is "BTC"
+    def CreateOrder_SellSaved(self, pair, saved_quantity, sell_rate):
+        sell_quantity = self.GetUserBalance(Exmo._1FromPair(pair)) - saved_quantity
+        return self._QueryPrivate("order_create", type="sell", pair=pair, quantity=sell_quantity, price=sell_rate)["order_id"]
+
+    # brief: creates an sell-order at current market sell-rate
+    def CreateOrder_SellMarket(self, pair, quantity):
+        return self._QueryPrivate("order_create", type="market_sell", pair=pair, quantity=quantity, price=0)["order_id"]
+
+    # brief: creates an sell-order at current market sell-rate and predefined total-cost
+    def CreateOrder_SellMarketTotal(self, pair, total_cost):
+        return self._QueryPrivate("order_create", type="market_sell_total", pair=pair, quantity=total_cost, price=0)["order_id"]
+
+    # brief: cancels the trade-order by its id
+    def CancelOrder(self, order_id):
+        return self._QueryPrivate("order_cancel", order_id=order_id)
+
+    # brief: creates stop-buy-order for predefined buy-rate
+    def CreateStopOrder_Buy(self, pair, buy_quantity, trigger_buy_rate):
+        return self._QueryPrivate("stop_market_order_create", type="buy", pair=pair, quantity=buy_quantity, trigger_price=trigger_buy_rate)
+
+    # brief: creates stop-sell-order for predefined sell-rate
+    def CreateStopOrder_Sell(self, pair, sell_quantity, trigger_sell_rate):
+        return self._QueryPrivate("stop_market_order_create", type="buy", pair=pair, quantity=sell_quantity, trigger_price=trigger_sell_rate)
+
+    # brief: cancels the stop-trade-order by its id
+    def CancelStopOrder(self, stop_order_id):
+        return self._QueryPrivate("stop_market_order_cancel", parent_order_id=stop_order_id)
+
+    # NOTE: next below class-methods are based on upper methods
+
+    # brief: checks is order open by its id
+    # param: order_id - target trade-order id
+    # return: true - if the order is open; false - vise versa
+    def IsOrderOpen(self, order_id):
+        for orders in self.GetUserOpenOrders().values():
+            for order in orders:
+                if int(order["order_id"]) == order_id:
+                    return True
+        return False
+
+    # brief: checks is order cancel by its id
+    # param: order_id - target trade-order id
+    # return: true - if the order is canceled; false - vise versa
+    def IsOrderCancel(self, order_id):
+        for orders in self.GetUserCancelledOrders():
+            for order in orders:
+                if int(order["order_id"]) == order_id:
+                    return True
+        return False
+
+    # brief: checks is order complete by its id
+    # param: order_id - target trade-order id
+    # return: true - if the order is completed; false - vise versa
+    def IsOrderComplete(self, pair, order_id):
+        for orders in self.GetUserDeals(pair).values():
+            for order in orders:
+                if int(order["order_id"]) == order_id:
+                    return True
+        return False
+
+    # brief: checking is buy-order
+    # param: order_id - target trade-order id
+    # return: true - if trade-order is buy-order
+    def IsItBuyOrder(self, order_id):
+        return self.GetOrderDeals(order_id)["type"] == "buy"
+
+    # brief: checking is sell-order
+    # param: order_id - target trade-order id
+    # return: true - if trade-order is buy-order
+    def IsItSellOrder(self, order_id):
+        return self.GetOrderDeals(order_id)["type"] == "sell"
+
+    # brief: gets cost of the trade-order by its id (for first currency in trade-pair)
+    # note1: if trade-pair is BTC_USD the cost will given for BTC
+    # param: order_id - target trade-order id
+    # return: cost of the tarde-order
+    def GetOrderCost1(self, order_id):
+        order_deals = self.GetOrderDeals(order_id)
+        if order_deals["type"] == "sell":
+            return float(order_deals["out_amount"])
+        elif order_deals["type"] == "buy":
+            return float(order_deals["in_amount"])
+        else:
+            raise "unspecified type of order: {}".format(order_deals)
+
+    # brief: gets cost of the trade-order by its id (for second currency in trade-pair)
+    # note1: if trade-pair is BTC_USD the cost will given for USD
+    # param: order_id - target trade-order id
+    # return: cost for the trade-order
+    def GetOrderCost2(self, order_id):
+        order_deals = self.GetOrderDeals(order_id)
+        if order_deals["type"] == "sell":
+            return float(order_deals["in_amount"])
+        elif order_deals["type"] == "buy":
+            return float(order_deals["out_amount"])
+        else:
+            raise "unspecified type of order: {}".format(order_deals)
+
+    # brief: gets rate of the trade-order by its id
+    # param: order_id - target trade-order id
+    # return: rate of the trade-order
+    def GetOrderRate(self, order_id):
+        order_deals = self.GetOrderDeals(order_id)
+        in_amount = float(order_deals["in_amount"])
+        out_amount = float(order_deals["out_amount"])
+        if order_deals["type"] == "sell":
+            return in_amount / out_amount
+        elif order_deals["type"] == "buy":
+            return out_amount / in_amount
+        else:
+            raise "unspecified type of order: {}".format(order_deals)
 
     # brief: gets list of the deals in currency pair
     @staticmethod
@@ -42,72 +223,17 @@ class Exmo:
     def GetCurrencyList():
         return Exmo.Query("currency")
 
-    # brief: gets information about user's account
-    def GetUserInfo(self):
-        return self.QueryPrivate("user_info")
+    # brief: gets commission for trading currency-pair
+    # return: the commission value for the currency-pair like: 1.0 - if commission is 0%; 0.996 - if commission is 0.4%
+    @staticmethod
+    def GetCommissionForPair(pair):
+        return 1 - float(Exmo.GetPairSettings()[pair]["commission_taker_percent"]) / 100
 
-    # brief: gets the list of user's open (active) orders
-    def GetUserOpenOrders(self):
-        return self.QueryPrivate("user_open_orders")
-
-    # brief: gets the list of user's candelled orders
-    def GetUserCancelledOrders(self, limit=100, offset=0):
-        return self.QueryPrivate("user_cancelled_orders", limit=limit, offset=offset)
-
-    # brief: gets the list of user's deals
-    def GetUserDeals(self, pair, limit=100, offset=0):
-        return self.QueryPrivate("user_trades", pair=pair, limit=limit, offset=offset)
-
-    # brief: gets the list of trade's deals
-    def GetOrderDeals(self, order_id):
-        return self.QueryPrivate("order_trades", order_id=order_id)
-
-    # brief: creates an buy-order
-    def CreateOrder_Buy(self, pair, buy_quantity, buy_rate):
-        return self.QueryPrivate("order_create", type="buy", pair=pair, quantity=buy_quantity, price=buy_rate)
-
-    # brief: creates an buy-order at current market buy-rate
-    def CreateOrder_BuyMarket(self, pair, buy_quantity):
-        return self.QueryPrivate("order_create", type="market_buy", pair=pair, quantity=buy_quantity, price=0)
-
-    # brief: creates an buy-order at current market buy-rate and predefined total-cost
-    def CreateOrder_BuyMarketTotal(self, pair, total_cost):
-        return self.QueryPrivate("order_create", type="market_buy_total", pair=pair, quantity=total_cost, price=0)
-
-    # brief: creates an sell-order
-    def CreateOrder_Sell(self, pair, sell_quantity, sell_rate):
-        return self.QueryPrivate("order_create", type="sell", pair=pair, quantity=sell_quantity, price=sell_rate)
-
-    # brief: creates an sell-order at current market sell-rate
-    def CreateOrder_SellMarket(self, pair, quantity):
-        return self.QueryPrivate("order_create", type="market_sell", pair=pair, quantity=quantity, price=0)
-
-    # brief: creates an sell-order at current market sell-rate and predefined total-cost
-    def CreateOrder_SellMarketTotal(self, pair, total_cost):
-        return self.QueryPrivate("order_create", type="market_sell_total", pair=pair, quantity=total_cost, price=0)
-
-    # brief: cancels the trade-order by its id
-    def CancelOrder(self, order_id):
-        return self.QueryPrivate("order_cancel", order_id=order_id)
-
-    # brief: creates stop-buy-order for predefined buy-rate
-    def CreateStopOrder_Buy(self, pair, buy_quantity, trigger_buy_rate):
-        return self.QueryPrivate("stop_market_order_create", type="buy", pair=pair, quantity=buy_quantity, trigger_price=trigger_buy_rate)
-
-    # brief: creates stop-sell-order for predefined sell-rate
-    def CreateStopOrder_Sell(self, pair, sell_quantity, trigger_sell_rate):
-        return self.QueryPrivate("stop_market_order_create", type="buy", pair=pair, quantity=sell_quantity, trigger_price=trigger_sell_rate)
-
-    # brief: cancels the stop-trade-order by its id
-    def CancelStopOrder(self, stop_order_id):
-        return self.QueryPrivate("stop_market_order_cancel", parent_order_id=stop_order_id)
-
-    # brief: performs https-get/post requests to private-API of Exmo-exchange
-    # param: api_method - the requested method
-    # param: params - params for the specifing of the method
-    # return: json-string with response on the requested method
-    def QueryPrivate(self, api_method, **params):
-        return Exmo.Query(api_method, self.__publick_key, self.__secret_key, **params)
+    # brief: gets precision for computing trade-rate for the trade-pair
+    # return: precision for trade-pair
+    @staticmethod
+    def GetPrecisionForPair(pair):
+        return int(Exmo.GetPairSettings()[pair]["price_precision"])
 
     # NOTE: for detailed information about structure of code in below see https://github.com/exmo-dev/exmo_api_lib/blob/master/rest/python/exmo3.py
 
@@ -143,3 +269,15 @@ class Exmo:
         if "error" in result and result["error"]:
             raise result["error"]
         return result
+
+    # brief: gets first currency from currency-pair
+    # return: first currency from currency-pair
+    @staticmethod
+    def _1FromPair(pair):
+        return pair.split('_')[0]
+
+    # brief: gets second currency from currency-pair
+    # return: second currency from currency-pair
+    @staticmethod
+    def _2FromPair(pair):
+        return pair.split('_')[1]
