9ff5fb4e7917ff5fc06ec91221ed0f1e7c913b10
Alexey Perestoronin <Alexey.Perestoronin@acronis.com>
2021-03-19 11:02:04 +0300

    v1.3.3.4: * ParamsManager was improved

--- tb_common/params_manager/__test_pm__.py
+++ tb_common/params_manager/__test_pm__.py
@@ -1,12 +1,18 @@
 import os
+import copy
 import unittest
 
 import tb_common.params_manager as pm
 import tb_common.params_manager.errors as errors
 
 
+current_dir = os.path.dirname(__file__)
+
+
+# brief: test saving and restoring for not None parameters
 class Test1(unittest.TestCase):
     def setUp(self):
+        self.save_filepath = os.path.join(current_dir, "save_file_Test1.txt")
         self._pm = pm.PM()
         self._pm.Add("param_1", default=1)
         self._pm.Add("param_2", default=2)
@@ -15,8 +21,27 @@ class Test1(unittest.TestCase):
         self._pm.Add("param_5", default=5)
 
     def test1(self):
+        save_dict = self._pm.SaveToDict()
+        pmr = copy.deepcopy(self._pm)
+        pmr.RestoreFromDict(save_dict)
+        self.assertEqual(self._pm.Get("param_1"), pmr.Get("param_1"))
+        self.assertEqual(self._pm.Get("param_2"), pmr.Get("param_2"))
+        self.assertEqual(self._pm.Get("param_3"), pmr.Get("param_3"))
+        self.assertEqual(self._pm.Get("param_4"), pmr.Get("param_4"))
+        self.assertEqual(self._pm.Get("param_5"), pmr.Get("param_5"))
+
+    def test2(self):
+        save_dict = self._pm.SaveToDict()
+        pmr = pm.PM.RestoreFromDictCls(save_dict)
+        self.assertEqual(self._pm.Get("param_1"), pmr.Get("param_1"))
+        self.assertEqual(self._pm.Get("param_2"), pmr.Get("param_2"))
+        self.assertEqual(self._pm.Get("param_3"), pmr.Get("param_3"))
+        self.assertEqual(self._pm.Get("param_4"), pmr.Get("param_4"))
+        self.assertEqual(self._pm.Get("param_5"), pmr.Get("param_5"))
+
+    def test3(self):
         save_string = self._pm.SaveToStr()
-        pmr = pm.PM()
+        pmr = copy.deepcopy(self._pm)
         pmr.RestoreFromStr(save_string)
         self.assertEqual(self._pm.Get("param_1"), pmr.Get("param_1"))
         self.assertEqual(self._pm.Get("param_2"), pmr.Get("param_2"))
@@ -24,68 +49,102 @@ class Test1(unittest.TestCase):
         self.assertEqual(self._pm.Get("param_4"), pmr.Get("param_4"))
         self.assertEqual(self._pm.Get("param_5"), pmr.Get("param_5"))
 
+    def test4(self):
+        save_string = self._pm.SaveToStr()
+        pmr = pm.PM.RestoreFromStrCls(save_string)
+        self.assertEqual(self._pm.Get("param_1"), pmr.Get("param_1"))
+        self.assertEqual(self._pm.Get("param_2"), pmr.Get("param_2"))
+        self.assertEqual(self._pm.Get("param_3"), pmr.Get("param_3"))
+        self.assertEqual(self._pm.Get("param_4"), pmr.Get("param_4"))
+        self.assertEqual(self._pm.Get("param_5"), pmr.Get("param_5"))
 
+    def test5(self):
+        self._pm.SaveToFile(self.save_filepath)
+        pmr = copy.deepcopy(self._pm)
+        pmr.RestoreFromFile(self.save_filepath)
+        self.assertEqual(self._pm.Get("param_1"), pmr.Get("param_1"))
+        self.assertEqual(self._pm.Get("param_2"), pmr.Get("param_2"))
+        self.assertEqual(self._pm.Get("param_3"), pmr.Get("param_3"))
+        self.assertEqual(self._pm.Get("param_4"), pmr.Get("param_4"))
+        self.assertEqual(self._pm.Get("param_5"), pmr.Get("param_5"))
+
+    def test6(self):
+        self._pm.SaveToFile(self.save_filepath)
+        pmr = pm.PM.RestoreFromFileCls(self.save_filepath)
+        self.assertEqual(self._pm.Get("param_1"), pmr.Get("param_1"))
+        self.assertEqual(self._pm.Get("param_2"), pmr.Get("param_2"))
+        self.assertEqual(self._pm.Get("param_3"), pmr.Get("param_3"))
+        self.assertEqual(self._pm.Get("param_4"), pmr.Get("param_4"))
+        self.assertEqual(self._pm.Get("param_5"), pmr.Get("param_5"))
+
+
+# brief: test saving and restoring for None parameters
 class Test2(unittest.TestCase):
     def setUp(self):
+        self.save_filepath = os.path.join(current_dir, "save_file_Test2.txt")
         self._pm = pm.PM()
         self._pm.Add("param_1")
         self._pm.Add("param_2")
 
     def test1(self):
+        save_dict = self._pm.SaveToDict()
+        pmr = copy.deepcopy(self._pm)
+        pmr.RestoreFromDict(save_dict)
+        self.assertEqual(self._pm.Get("param_1"), None)
+        self.assertEqual(pmr.Get("param_1"), None)
+        self.assertEqual(self._pm.Get("param_2"), None)
+        self.assertEqual(pmr.Get("param_2"), None)
+
+    def test2(self):
+        save_dict = self._pm.SaveToDict()
+        pmr = pm.PM.RestoreFromDictCls(save_dict)
+        self.assertEqual(self._pm.Get("param_1"), None)
+        self.assertEqual(pmr.Get("param_1"), None)
+        self.assertEqual(self._pm.Get("param_2"), None)
+        self.assertEqual(pmr.Get("param_2"), None)
+
+    def test3(self):
         save_string = self._pm.SaveToStr()
-        pmr = pm.PM()
+        pmr = copy.deepcopy(self._pm)
         pmr.RestoreFromStr(save_string)
         self.assertEqual(self._pm.Get("param_1"), None)
         self.assertEqual(pmr.Get("param_1"), None)
         self.assertEqual(self._pm.Get("param_2"), None)
         self.assertEqual(pmr.Get("param_2"), None)
 
+    def test4(self):
+        save_string = self._pm.SaveToStr()
+        pmr = pm.PM.RestoreFromStrCls(save_string)
+        self.assertEqual(self._pm.Get("param_1"), None)
+        self.assertEqual(pmr.Get("param_1"), None)
+        self.assertEqual(self._pm.Get("param_2"), None)
+        self.assertEqual(pmr.Get("param_2"), None)
 
-class Test3(unittest.TestCase) :
-    def setUp(self) :
-        self._pm = pm.PM()
-        self._pm.Add("param_1", default=1)
-        self._pm.Add("param_2", default=2)
-        self._pm.Add("param_3", default=3)
-        self._pm.Add("param_4", default=4)
-        self._pm.Add("param_5", default=5)
+    def test5(self):
+        self._pm.SaveToFile(self.save_filepath)
+        pmr = copy.deepcopy(self._pm)
+        pmr.RestoreFromFile(self.save_filepath)
+        self.assertEqual(self._pm.Get("param_1"), None)
+        self.assertEqual(pmr.Get("param_1"), None)
+        self.assertEqual(self._pm.Get("param_2"), None)
+        self.assertEqual(pmr.Get("param_2"), None)
 
-    def test1(self):
-        save_filepath = os.path.join(os.path.split(__file__)[0], "save_file.txt")
-        self._pm.SaveToFile(save_filepath)
-        pmr = pm.PM()
-        pmr.RestoreFromFile(save_filepath)
-        self.assertEqual(self._pm.Get("param_1"), pmr.Get("param_1"))
-        self.assertEqual(self._pm.Get("param_2"), pmr.Get("param_2"))
-        self.assertEqual(self._pm.Get("param_3"), pmr.Get("param_3"))
-        self.assertEqual(self._pm.Get("param_4"), pmr.Get("param_4"))
-        self.assertEqual(self._pm.Get("param_5"), pmr.Get("param_5"))
-
-
-class Test4(unittest.TestCase) :
-    def setUp(self) :
-        self._pm = pm.PM()
-        self._pm.Add("param_1")
-        self._pm.Add("param_2")
-
-    def test1(self):
-        save_filepath = os.path.join(os.path.split(__file__)[0], "save_file.txt")
-        self._pm.SaveToFile(save_filepath)
-        pmr = pm.PM()
-        pmr.RestoreFromFile(save_filepath)
+    def test6(self):
+        self._pm.SaveToFile(self.save_filepath)
+        pmr = pm.PM.RestoreFromFileCls(self.save_filepath)
         self.assertEqual(self._pm.Get("param_1"), None)
         self.assertEqual(pmr.Get("param_1"), None)
         self.assertEqual(self._pm.Get("param_2"), None)
         self.assertEqual(pmr.Get("param_2"), None)
 
 
-class Test5(unittest.TestCase):
-    def setUp(self) :
+# brief: testing of the error raising during of checking parameters
+class Test3(unittest.TestCase):
+    def test1(self):
         self._pm = pm.PM()
         self._pm.Add("param_1")
         self._pm.Add("param_2")
-
-    def test1(self):
+        # testing
         self.assertRaises(errors.ParameterIsAlreadyExist, self._pm.Add, "param_1")
         self.assertRaises(errors.ParameterIsAlreadyExist, self._pm.Add, "param_2")
         self.assertRaises(errors.ParameterIsNotExist, self._pm.Set, "param_3", 3)
@@ -93,6 +152,119 @@ class Test5(unittest.TestCase):
         self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check)
         self._pm.Set("param_1", 1)
         self._pm.Set("param_2", 2)
+        self._pm.Check()
+
+    def test2(self):
+        self._pm = pm.PM()
+        self._pm.Add("param_1", demanding_indicator=pm.PM.DI.ORDER_1.Key)
+        self._pm.Add("param_2", demanding_indicator=pm.PM.DI.ORDER_2.Key)
+        self._pm.Add("param_3", demanding_indicator=pm.PM.DI.ORDER_3.Key)
+        self._pm.Add("param_4", demanding_indicator=pm.PM.DI.ORDER_4.Key)
+        self._pm.Add("param_5", demanding_indicator=pm.PM.DI.ORDER_5.Key)
+        # testing
+        self._pm.Check(pm.PM.DI.NEVER.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ALWAYS.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_1.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_2.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_3.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_4.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_5.Key)
+        self._pm.Set("param_1", 1)
+        self._pm.Check(pm.PM.DI.ORDER_1.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ALWAYS.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_2.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_3.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_4.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_5.Key)
+        self._pm.Set("param_2", 2)
+        self._pm.Check(pm.PM.DI.ORDER_1.Key)
+        self._pm.Check(pm.PM.DI.ORDER_2.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ALWAYS.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_3.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_4.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_5.Key)
+        self._pm.Set("param_3", 3)
+        self._pm.Check(pm.PM.DI.ORDER_1.Key)
+        self._pm.Check(pm.PM.DI.ORDER_2.Key)
+        self._pm.Check(pm.PM.DI.ORDER_3.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ALWAYS.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_4.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_5.Key)
+        self._pm.Set("param_4", 4)
+        self._pm.Check(pm.PM.DI.ORDER_1.Key)
+        self._pm.Check(pm.PM.DI.ORDER_2.Key)
+        self._pm.Check(pm.PM.DI.ORDER_3.Key)
+        self._pm.Check(pm.PM.DI.ORDER_4.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ALWAYS.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_5.Key)
+        self._pm.Set("param_5", 5)
+        self._pm.Check(pm.PM.DI.ORDER_1.Key)
+        self._pm.Check(pm.PM.DI.ORDER_2.Key)
+        self._pm.Check(pm.PM.DI.ORDER_3.Key)
+        self._pm.Check(pm.PM.DI.ORDER_4.Key)
+        self._pm.Check(pm.PM.DI.ORDER_5.Key)
+        self._pm.Check(pm.PM.DI.ALWAYS.Key)
+        self._pm.Check()
+
+    def test3(self):
+        self._pm = pm.PM()
+        self._pm.Add("param_1", demanding_indicator=pm.PM.DI.NEVER.Key)
+        self._pm.Add("param_2", demanding_indicator=pm.PM.DI.NEVER.Key)
+        self._pm.Add("param_3", demanding_indicator=pm.PM.DI.NEVER.Key)
+        self._pm.Add("param_4", demanding_indicator=pm.PM.DI.NEVER.Key)
+        self._pm.Add("param_5", demanding_indicator=pm.PM.DI.NEVER.Key)
+        # testing
+        self._pm.Check(pm.PM.DI.NEVER.Key)
+        self._pm.Check(pm.PM.DI.ORDER_1.Key)
+        self._pm.Check(pm.PM.DI.ORDER_2.Key)
+        self._pm.Check(pm.PM.DI.ORDER_3.Key)
+        self._pm.Check(pm.PM.DI.ORDER_4.Key)
+        self._pm.Check(pm.PM.DI.ORDER_5.Key)
+        self._pm.Check(pm.PM.DI.ALWAYS.Key)
+
+    def test4(self):
+        self._pm = pm.PM()
+        self._pm.Add("param_1", demanding_indicator=pm.PM.DI.ALWAYS.Key)
+        self._pm.Add("param_2", demanding_indicator=pm.PM.DI.ALWAYS.Key)
+        self._pm.Add("param_3", demanding_indicator=pm.PM.DI.ALWAYS.Key)
+        self._pm.Add("param_4", demanding_indicator=pm.PM.DI.ALWAYS.Key)
+        self._pm.Add("param_5", demanding_indicator=pm.PM.DI.ALWAYS.Key)
+        # testing
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_5.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_4.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_3.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_2.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_1.Key)
+        self._pm.Set("param_1", 1)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_5.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_4.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_3.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_2.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_1.Key)
+        self._pm.Set("param_2", 2)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_5.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_4.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_3.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_2.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_1.Key)
+        self._pm.Set("param_3", 3)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_5.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_4.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_3.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_2.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_1.Key)
+        self._pm.Set("param_4", 4)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_5.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_4.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_3.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_2.Key)
+        self.assertRaises(errors.RequiredParameterIsNotInitialized, self._pm.Check, pm.PM.DI.ORDER_1.Key)
+        self._pm.Set("param_5", 5)
         self._pm.Check()
 
 
--- tb_common/params_manager/pm.py
+++ tb_common/params_manager/pm.py
@@ -2,47 +2,111 @@ import json
 
 import common.faf as faf
 
+from common import CREATE_CONSTANT as CC
+
 import tb_common.params_manager.errors as errors
 
 # brief: class for ma
 # note1: PM - is a Parameters Manger abbreviation
 class PM:
+    class PARAMS:
+        VALUE = CC("PARAMETER'S_VALUE")
+        DEMAND = CC("PARAMETER'S_DEMAND")
+
+    # brief: collects demand indicators
+    # note: DI is Demanding Indicator
+    class DI:
+        ALWAYS = CC('inf')
+        ORDER_0 = CC('0')
+        ORDER_1 = CC('1')
+        ORDER_2 = CC('2')
+        ORDER_3 = CC('3')
+        ORDER_4 = CC('4')
+        ORDER_5 = CC('5')
+        NEVER = CC('nan')
+
+
     def __init__(self):
         self._parameters = {
-            # (object)name : [(object)value, (bool)is_required]
+            # (object)name : [
+            #   PARAMETER'S_VALUE : (object)value,
+            #   PARAMETER'S_DEMAND : (str)demanding_indicator
+            # ]
         }
 
-    def Add(self, name, *, default=None, is_required=True):
+    # brief: add new parameter
+    # param: name - the name of the new parameter
+    # param: default - its default value
+    # param: demanding_indicator - its demanding value
+    def Add(self, name, *, default=None, demanding_indicator=DI.ALWAYS.Key):
         if name in self._parameters.keys():
             raise errors.ParameterIsAlreadyExist()
-        self._parameters[name] = [default, is_required]
+        self._parameters[name] = {
+            PM.PARAMS.VALUE.Key : default,
+            PM.PARAMS.DEMAND.Key : demanding_indicator
+        }
 
     def Get(self, name):
         if name not in self._parameters.keys():
             raise errors.ParameterIsNotExist()
-        return self._parameters[name][0]
+        return self._parameters[name][PM.PARAMS.VALUE.Key]
 
-    def Set(self, name, value, is_required=True):
+    def Set(self, name, value):
         if name not in self._parameters.keys():
             raise errors.ParameterIsNotExist()
-        self._parameters[name] = [value, is_required]
+        self._parameters[name][PM.PARAMS.VALUE.Key] = value
 
-    def Check(self):
+    def Check(self, demanding_indicator=DI.ALWAYS.Key):
+        if demanding_indicator == PM.DI.NEVER.Key:
+            return
         for name, param in self._parameters.items():
-            value, is_required = param
-            if is_required and value is None:
-                error = errors.RequiredParameterIsNotInitialized()
-                error.SetName(name)
-                raise error
+            value = param[PM.PARAMS.VALUE.Key]
+            demand = param[PM.PARAMS.DEMAND.Key]
+            if value is not None or demand == PM.DI.NEVER.Key:
+                continue
+            if demand != PM.DI.ALWAYS.Key and float(demand) > float(demanding_indicator):
+                continue
+            error = errors.RequiredParameterIsNotInitialized()
+            error.SetName(name)
+            raise error
 
-    def SaveToStr(self):
-        return json.dumps(self._parameters, indent=4)
+    def GetCopyParameters(self):
+        import copy
+        return copy.deepcopy(self._parameters)
+
+    # NOTE: Save and Restore
+
+    def SaveToDict(self):
+        return self.GetCopyParameters()
+
+    def RestoreFromDict(self, save_dict):
+        for name, params in save_dict.items():
+            value = params[PM.PARAMS.VALUE.Key]
+            self.Set(name, value)
+
+    @classmethod
+    def RestoreFromDictCls(cls, save_dict):
+        restored_pm = cls()
+        restored_pm._parameters = save_dict
+        return restored_pm
+
+    def SaveToStr(self, indent=4):
+        return json.dumps(self.SaveToDict(), indent=indent)
 
     def RestoreFromStr(self, save_string):
-        self._parameters = dict(json.loads(save_string))
+        self.RestoreFromDict(dict(json.loads(save_string)))
+
+    @classmethod
+    def RestoreFromStrCls(cls, save_string):
+        saved_dict = json.loads(save_string)
+        return cls.RestoreFromDictCls(saved_dict)
 
     def SaveToFile(self, filepath):
         faf.SaveContentToFile1(filepath, self.SaveToStr())
 
     def RestoreFromFile(self, filepath):
         self.RestoreFromStr(faf.GetFileContent(filepath))
+
+    @classmethod
+    def RestoreFromFileCls(cls, filepath):
+        return cls.RestoreFromStrCls(faf.GetFileContent(filepath))
